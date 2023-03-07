bab2f59cd6d98dab352a81a1e61f4f1fa90da002
eneilson lima oliveira <eneilson6@gmail.com>
2023-03-07 01:22:46 -0300

    Create of Dashboard

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,8 +1,67 @@
 import React from "react"
 
+import "./Home.css"
 const Home = () => {
-  return(
-    <div>Home // Dashboard</div>
+  return (
+
+    <main className="main">
+      <div className="box ">
+        <div className="top">Status</div>
+        <div className="middle">
+          <img src="./assets/carinha.svg" alt="bolinha verde sorrindo" />
+          <span>Score:75%</span> <br />
+        </div>
+        <div className="bottom">
+          <span>Cursos:85 </span>
+          <span>Alunos:121</span>
+        </div>
+
+      </div>
+      <div className="box sell grid">
+        <div className="top">Cursos</div>
+        <div className="middle">
+          <svg viewBox="0 0 142 142" >
+            <circle
+              cx="50%"
+              cy="50%"
+              r="60"
+              opacity="0.1"
+              stroke="#040419"
+            />
+            <circle
+              cx="50%"
+              cy="50%"
+              r="60"
+              stroke="url(#paint0_linear_201_104)"
+            />
+            <defs>
+              <linearGradient id="paint0_linear_201_104" x1="1.97421e-07" y1="82" x2="154" y2="178" gradientUnits="userSpaceOnUse">
+                <stop stop-color="#DF9780" />
+                <stop offset="1" stop-color="#A66DE9" />
+              </linearGradient>
+            </defs>
+          </svg>
+        </div>
+        <div className="bottom">
+          <div className="item">
+            <span>Alcançado</span>
+          </div>
+        </div>
+      </div>
+      <div className="box target grid">
+        <div className="top">Alunos</div>
+        <div className="middle">
+
+        </div>
+        <div className="bottom">
+          <div className="item">
+            <span>Alcançado</span>
+          </div>
+        </div>
+      </div>
+      <div className="box">test</div>
+    </main>
+
   )
 }
 export default Home
--- src/routes/NewPost.jsx
+++ src/routes/NewPost.jsx
@@ -1,50 +1,40 @@
 import React from 'react'
-  import "./Register.css"
-  import { useForm } from 'react-hook-form';
- const NewPost = () => {
-    const { register, handleSubmit, setValue, setFocus } = useForm();
-    const onSubmit = (e) => {
-      console.log(e)
-    }
-    const checkCep = (e) => {
-  
-      const cep = e.target.value.replace(/\D/g, '');
-  
-      console.log(cep);
-      fetch(`https://viacep.com.br/ws/${cep}/json/ `)
-        .then(res => res.json())
-        .then(data => {
-          console.log(data);
-          setValue('address', data.logradouro);
-          setValue('neighborhood', data.bairro);
-          setValue('city', data.localidade);
-          setValue('uf', data.uf);
-          setFocus('addressNumber')
-        });
-    }
-    return (
-  
-      <form onSubmit={handleSubmit(onSubmit)} className='form'>
-        <h2 className="h2">Cadastro de Cursos</h2>
-        <div className="form-left-full">
-          <label>
-            <input type="text" {...register("id")} placeholder='ID' />
-          </label>
-        </div>
-        <div className="form-left-todo">
-          <label>
-            <input type="text" {...register("nome")} placeholder='Nome' />
-          </label>
-        </div>
-        <div className="form-btn" >
-          <button type="reset">Cancelar</button>
-          <button type="submit" >Gravar</button>
-  
-        </div>
-        </form >
-        )
+import "./Register.css"
+import { useForm } from 'react-hook-form';
+const NewPost = () => {
+  const { register, handleSubmit, setValue, setFocus } = useForm();
+  const onSubmit = (e) => {
+    console.log(e)
   }
-       
-       
-   export default NewPost
+  const checkCep = (e) => {
+
+    const cep = e.target.value.replace(/\D/g, '');
+
+
+  }
+  return (
+
+    <form onSubmit={handleSubmit(onSubmit)} className='form'>
+      <h2 className="h2">Cadastro de Cursos</h2>
+      <div className="form-left-full">
+        <label>
+          <input type="text" {...register("id")} placeholder='ID' />
+        </label>
+      </div>
+      <div className="form-left-todo">
+        <label>
+          <input type="text" {...register("nome")} placeholder='Nome' />
+        </label>
+      </div>
+      <div className="form-btn" >
+        <button type="reset">Cancelar</button>
+        <button type="submit" >Gravar</button>
+
+      </div>
+    </form >
+  )
+}
+
+
+export default NewPost
 
